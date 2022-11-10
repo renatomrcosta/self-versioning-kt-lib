a0b4051ed4749c721cb6fc22a07312e024e830b6
Renato Costa <renato@closelink.net>
2022-11-10 20:59:25 +0100

    docs: add license

--- src/main/kotlin/com/xunfos/lib/utils.kt
+++
@@ -1,8 +0,0 @@
-package com.xunfos.lib
-
-import kotlinx.coroutines.delay
-
-suspend fun <T> withDelay(timeoutMillis: Long = 100L, block: () -> T): T {
-    delay(timeoutMillis)
-    return block()
-}
